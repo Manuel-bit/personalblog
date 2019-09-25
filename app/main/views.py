e759bcddbf027049fc28ba81fb8e094eff239577
manuel.bit <emmanuel_njuguna@outlook.com>
2019-09-25 18:11:35 +0300

    add images to the static folder

--- app/main/views.py
+++ app/main/views.py
@@ -3,14 +3,22 @@ from . import main
 from .forms import NewBlogForm
 from ..models import Blog
 from .. import db
+from flask_login import login_required,current_user
+
+@main.route('/')
+def index():
+  
+  return render_template('index.html')
 
 @main.route('/home')
+@login_required
 def home():
 
   blogs= Blog.query.all()
   return render_template('home.html',blogs=blogs)
 
 @main.route('/newblog', methods =["GET", "POST"])
+@login_required
 def newblog():
   form = NewBlogForm()
   if form.validate_on_submit():
@@ -20,7 +28,3 @@ def newblog():
     return redirect(url_for('main.home'))
   return render_template('newblog.html',form=form)
 
-@main.route('/')
-def index():
-  
-  return render_template('index.html')
